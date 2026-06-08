c7bde96f4b9b113a365f7de6292511ff5fdb9732
MinhTrung07 <ft.trung0902@gmail.com>
2026-06-08 21:53:54 +0700

    feat(case 1): create feature 1 with add_stock

---
+++ feature1.py
@@ -0,0 +1,25 @@
+def conver_user_input_to_int(content:str) -> int:
+    while True:
+        try:
+            user_input = int(input(f"{content}"))
+
+        except ValueError:
+            print("Không hợp lệ, vui lòng nhập lại")
+        
+        else: 
+            return user_input
+
+inventory_stock = 100
+
+def add_stock(amount):
+ 
+    if(amount <= 0):
+        print("Dữ liệu nhập vào phải lớn hơn 0.") 
+
+    global inventory_stock
+    inventory_stock += amount
+    
+    print(f"Đã thêm thành công {amount} sản phẩm")
+    print(f"Tồn kho hiện tại: {inventory_stock}")
+
+
